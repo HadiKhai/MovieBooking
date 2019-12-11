460c689f1dfde53fd47cbb24b5596b04dee242af
HadiKhair <hadikhairallah@outlook.com>
2019-12-11 22:25:18 +0200

    seats working

--- src/app/component/movie-cinemas-rooms-seats/movie-cinemas-rooms-seats.component.ts
+++ src/app/component/movie-cinemas-rooms-seats/movie-cinemas-rooms-seats.component.ts
@@ -14,6 +14,9 @@ export class MovieCinemasRoomsSeatsComponent implements OnInit {
   seats;
   seatsArray: any[];
   seatsArraySorted: any[][];
+  imgSrc: string = "../assets/img/seat.png";
+  seatId;
+  seatsBooleanArray = [];
   constructor(
     private seatService: SeatService,
     private route: ActivatedRoute
@@ -36,13 +39,42 @@ export class MovieCinemasRoomsSeatsComponent implements OnInit {
       this.seatsArraySorted[i] = [];
       if (this.seats.length === 100) {
         for (let j = 0; j < 10; j++) {
-          this.seatsArraySorted[i][j] = this.seatsArray[i + j];
+          this.seatsArraySorted[i][j] = this.seatsArray[i * 10 + j];
         }
       }
     }
+    for (let i = 0; i < this.seatsArray.length; i++) {
+      this.seatsBooleanArray.push(false);
+    }
     console.log(this.seatsArraySorted);
   }
-  printSeatId(seatId) {
-    console.log(seatId);
+  toggleComponent(id) {
+    let index;
+    for (let i = 0; i < this.seatsArray.length; i++) {
+      if (id === this.seatsArray[i].seatId) {
+        index = i;
+      }
+    }
+    if (this.seatsBooleanArray[index] === true) {
+      this.seatsBooleanArray[index] = false;
+    } else {
+      for (let i = 0; i < this.seatsBooleanArray.length; i++) {
+        this.seatsBooleanArray[i] = false;
+      }
+      this.seatsBooleanArray[index] = true;
+    }
+  }
+  toggleC(seatId) {
+    let index;
+    for (let i = 0; i < this.seatsArray.length; i++) {
+      if (seatId === this.seatsArray[i].seatId) {
+        index = i;
+      }
+    }
+    return this.seatsBooleanArray[index];
+  }
+  saveSeatId(seatId) {
+    this.seatId = seatId;
+    console.log(this.seatId);
   }
 }
